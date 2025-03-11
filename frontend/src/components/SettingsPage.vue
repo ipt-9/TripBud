99f38abfa856ac0fa4be5ac98849cceddcc85a5f
Sandro Sliskovic <sandro_sliskovic@sluz.ch>
2025-03-11 14:29:11 +0100

    added SettingsPage component

---
+++ frontend/src/components/SettingsPage.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="login-container">
+      <div class="logo-container">
+        <img src="src/assets/TripBudLogo.png" alt="TripBud Logo" class="logo" />
+        <span class="logo-text">TripBud</span>
+      </div>
+    </div>
+  </template>
