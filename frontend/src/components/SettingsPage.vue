5d6014c84c0a561b6e4e3015632a30de007f342d
Sandro Sliskovic <sandro_sliskovic@sluz.ch>
2025-03-25 16:33:06 +0100

    finished account register of settings component

--- frontend/src/components/SettingsPage.vue
+++ frontend/src/components/SettingsPage.vue
@@ -6,9 +6,13 @@
         </div>
         <div class="rounded-box-left">
             <input type="text" placeholder="Search...">
-            <a href=""><h2>Account</h2></a>
-            <a href=""><h2>Plans</h2></a>
-            <a href=""><h2>Help & Support</h2></a>
+            <a href="">
+              <img src="../assets/user.png"/><h2>Account</h2></a>
+            <a href="">
+              <img src="../assets/shopping-cart.png"/><h2>Plans</h2></a>
+            <a href="">
+              <img src="../assets/info.png"/><h2>Help & Support</h2></a>
+            <button class="box-left-logout">Log Out</button>
 
         </div>
         <div class ="rounded-box-right">
@@ -20,6 +24,9 @@
               <div class="box-right-profile">
               <h1>Your Profile</h1>
               <p>Update your profile settings here</p>
+              <img src="../assets/account-symbol.png" class="profile-picture"/>
+                <button class="box-right-edit-button">Edit</button>
+                <button class="box-right-delete-button">Delete</button>
             </div>
           </div>
             
@@ -137,10 +144,30 @@ export default {
     box-sizing: border-box;
 }
 
+.rounded-box-left img {
+  height: 30px;
+  margin-right: 10px; /* Abstand zwischen Bild und Text */
+  margin-bottom: 45px;
+  margin-right: 15px;
+}
+
 .rounded-box-left a {
   text-decoration: none;
-  color: inherit;
-  cursor: pointer;
+    color: inherit;
+    cursor: pointer;
+    display: flex;
+    align-items: center; /* Bild und Text vertikal zentrieren */
+    margin-bottom: 20px; /* Abstand zwischen den Links */
+}
+
+.box-left-logout {
+  width: 100%;
+  height: 6%;
+  border-radius: 50px;
+  background-color: rgb(105, 105, 105);
+  border: none;
+  color: white;
+  font-size: 110%;
 }
 
 .rounded-box-right {
@@ -183,6 +210,62 @@ export default {
     margin-top: 35px; /* Abstand nach unten */
     display: inline-block; /* Damit margin funktioniert */
     font-weight: 500;
+}
+
+.box-right-buttons {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 60px;
+  align-items: center;
+}
+
+.box-right-profile img {
+  height: 80px;
+  margin-right: 50px;
+  position: absolute;
+  margin-top: 50px;
+  top: 10%;
+  right: 20%;
+
+}
+
+.profile-picture {
+  width: 90px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+
+
+.box-right-edit-button {
+  background-color: #409FDB;
+  border-radius: 50px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  right: 15%;
+  top: 10%;
+  margin-top: 70px;
+  margin-right: 10px;
+  height: 4.5vh;
+  width: 4vw;
+  font-size: 105%;
+
+}
+
+.box-right-delete-button {
+  background-color: #db4040;
+  border-radius: 50px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  position: absolute;
+  top: 10%;
+  right: 8%;
+  margin-top: 70px;
+  height: 4.5vh;
+  width: 5vw;
+  font-size: 105%;
 }
 
 .box-right-line {
